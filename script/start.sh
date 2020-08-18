0fafe46ca2caf1f5d2719133e8dd19f4615370c3
wangrs <wangrsa@dcits.com>
2020-08-18 09:45:42 +0800

    日志文件名称

--- script/start.sh
+++ script/start.sh
@@ -3,4 +3,4 @@
 BASE_HOME=$(cd $(dirname $0) && pwd)
 export BASE_HOME
 echo "BASE_HOME is "$BASE_HOME
-nohup ${JAVA_HOME}/bin/java -Dspring.config.location=${BASE_HOME}/application.properties -jar zabbix-agent-api-service-0.0.1.jar > cop.log 2>&1 &
+nohup ${JAVA_HOME}/bin/java -Dspring.config.location=${BASE_HOME}/application.properties -jar zabbix-agent-api-service-0.0.1.jar > zabbixagentapiservice.log 2>&1 &
