f210d3606ae68a9fe914f7fde0da199b9719c245
wangrs <wangrsa@dcits.com>
2020-08-20 09:47:14 +0800

    删除tar

--- tar/zabbix-agent-api-service/stop.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-pn=`ps -ef|grep -i "zabbix-agent-api-service-0.0.1.jar"|grep -v grep|awk '{print $2}'`
-if [ -n "$pn" ];then
-echo "killing smartcop process,excute kill -9 $pn"
-
-kill -9 $pn
-
-fi
