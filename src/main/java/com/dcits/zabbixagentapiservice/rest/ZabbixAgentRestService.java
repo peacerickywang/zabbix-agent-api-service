560459be0e84ff492d816bcd3f033320a05bc636
wangrs <wangrsa@dcits.com>
2020-11-12 09:01:34 +0800

    新增测试接口

--- src/main/java/com/dcits/zabbixagentapiservice/rest/ZabbixAgentRestService.java
+++ src/main/java/com/dcits/zabbixagentapiservice/rest/ZabbixAgentRestService.java
@@ -7,9 +7,12 @@ import com.dcits.zabbixagentapiservice.util.ActionFactory;
 import com.huawei.esdk.fusioncompute.local.ServiceFactory;
 import com.huawei.esdk.fusioncompute.local.model.ClientProviderBean;
 import com.huawei.esdk.fusioncompute.local.model.FCSDKResponse;
+import com.huawei.esdk.fusioncompute.local.model.PageList;
 import com.huawei.esdk.fusioncompute.local.model.common.LoginResp;
+import com.huawei.esdk.fusioncompute.local.model.net.PortGroup;
 import com.huawei.esdk.fusioncompute.local.model.site.SiteBasicInfo;
 import com.huawei.esdk.fusioncompute.local.resources.common.AuthenticateResource;
+import com.huawei.esdk.fusioncompute.local.resources.net.PortGroupResource;
 import com.huawei.esdk.fusioncompute.local.resources.site.SiteResource;
 import io.swagger.annotations.ApiOperation;
 import org.slf4j.Logger;
@@ -129,5 +132,46 @@ public class ZabbixAgentRestService {
             return Float.parseFloat(targetAction.doAction(clientProvider, siteBasicInfoList, zabbixAgentQueryInfo.getInstanceName(), zabbixAgentQueryInfo.getMetric()));
         };
         return callable;
+    }
+
+    @ApiOperation("测试接口")
+    @RequestMapping(value = "/test", method = RequestMethod.PUT)
+    @ResponseBody
+    public String test(@RequestBody ZabbixAgentQueryInfo zabbixAgentQueryInfo, HttpServletRequest request, HttpServletResponse response) {
+        logger.info("ACTION: test");
+        // 设定服务器配置
+        ClientProviderBean clientProvider = new ClientProviderBean();
+        // 设定服务器配置_设定服务器IP
+        clientProvider.setServerIp(zabbixAgentQueryInfo.getServerIP());
+        // 设定服务器配置_设定服务器端口号
+        clientProvider.setServerPort(serverPort);
+        clientProvider.setUserName(zabbixAgentQueryInfo.getUsername());
+        clientProvider.setVersion(Float.parseFloat(zabbixAgentQueryInfo.getVersion()));
+        // 初始化用户资源实例
+        AuthenticateResource auth = ServiceFactory.getService(AuthenticateResource.class, clientProvider);
+        // 以用户名，用户密码作为传入参数，调用AuthenticateResource提供的login方法，完成用户的登录
+        FCSDKResponse<LoginResp> resp = new FCSDKResponse<LoginResp>();
+        try {
+            resp = auth.login(zabbixAgentQueryInfo.getUsername(), new String(Base64.getDecoder().decode(zabbixAgentQueryInfo.getPassword()), "utf-8"));
+            if (!resp.getErrorCode().equals(ERROR_CODE)) {
+                logger.error("登录失败，服务器连接认证失败！登陆信息：" + JSON.toJSONString(clientProvider));
+            }
+        } catch (Exception e) {
+            logger.error(e.getMessage());
+        }
+        // 获取SiteResource接口的实现
+        SiteResource site = ServiceFactory.getService(SiteResource.class, clientProvider);
+        FCSDKResponse<List<SiteBasicInfo>> resps = site.querySites();
+        if (!resps.getErrorCode().equals(ERROR_CODE)) {
+            logger.error("获取站点信息失败！登陆信息：" + JSON.toJSONString(clientProvider));
+        }
+        List<SiteBasicInfo> siteBasicInfoList = resps.getResult();
+        // 获取PortGroupResource接口的实现
+        PortGroupResource portGroupResource = ServiceFactory.getService(PortGroupResource.class, clientProvider);
+        //通过switch uri获取该交换机下的所有端口组（网络）
+        FCSDKResponse<PageList<PortGroup>> portGroups = portGroupResource
+                .queryPortGroups("/service/sites/48DF087C/dvswitchs/4", null, null, null, null, null);
+        logger.info(JSON.toJSONString(portGroups));
+        return JSON.toJSONString(portGroups);
     }
 }
