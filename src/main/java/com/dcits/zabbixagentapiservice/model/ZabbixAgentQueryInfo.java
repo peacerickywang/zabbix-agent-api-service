8a41ef5acb14e6b44cf15a352a8201bb48467af1
wangrs <wangrsa@dcits.com>
2021-01-13 09:23:16 +0800

    zabbixAgent_FusionCompute

--- src/main/java/com/dcits/zabbixagentapiservice/model/ZabbixAgentQueryInfo.java
+++ src/main/java/com/dcits/zabbixagentapiservice/model/ZabbixAgentQueryInfo.java
@@ -1,5 +1,8 @@
 package com.dcits.zabbixagentapiservice.model;
 
+import lombok.Data;
+
+@Data
 public class ZabbixAgentQueryInfo {
     //server IP
     String serverIP;
@@ -15,60 +18,4 @@ public class ZabbixAgentQueryInfo {
     String instanceName;
     //监控项
     String metric;
-
-    public String getServerIP() {
-        return serverIP;
-    }
-
-    public void setServerIP(String serverIP) {
-        this.serverIP = serverIP;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public String getVersion() {
-        return version;
-    }
-
-    public void setVersion(String version) {
-        this.version = version;
-    }
-
-    public String getAction() {
-        return action;
-    }
-
-    public void setAction(String action) {
-        this.action = action;
-    }
-
-    public String getInstanceName() {
-        return instanceName;
-    }
-
-    public void setInstanceName(String instanceName) {
-        this.instanceName = instanceName;
-    }
-
-    public String getMetric() {
-        return metric;
-    }
-
-    public void setMetric(String metric) {
-        this.metric = metric;
-    }
 }
